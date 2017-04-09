38e0dc3d6a366020809c4c3906db8ceb7b460e19
igabidev <igabidev@gmail.com>
2017-04-09 19:28:24 +0300

    Need to do some changes...

---
+++ script.js
@@ -0,0 +1,9 @@
+
+var x = ["+","-","*","/"];
+function badCarracters(){
+	for (var i in x) {
+		if(x.charAt(i) ==="-")
+	}
+}//not working...
+//trebuie sa ma gandesc la o functie care sa imi elimine ultimul caracter atunci cand adaug alt caracter matematic si sa nu imi accept "string"
+
